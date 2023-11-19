895f3b231af132b87a1d452a61d476efef79210b
Somai Mahmoud <72657759+mahmoud-somai@users.noreply.github.com>
2023-11-19 12:57:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/',(req,res)=>{
     res.json('Another Update Backend From Me Mahmoud');
 }
 );
+app.get('/api',(req,res)=>{
+    res.json('test this end Point');
+}
+);
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
