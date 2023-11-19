30edafff1922404cd55072e025c23fcdbdb79a1f
Somai Mahmoud <72657759+mahmoud-somai@users.noreply.github.com>
2023-11-19 13:00:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.json('Another Update Backend ');
+    res.json('Another Update  ');
 }
 );
 app.get('/api',(req,res)=>{
