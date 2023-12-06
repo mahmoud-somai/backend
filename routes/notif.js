f191456f3b327e5697fe8ebfcd0d291daf555191
MahmoudSomai <somai.mahmoud096@gmail.com>
2023-12-06 23:50:21 +0100

    anothed update

--- routes/notif.js
+++ routes/notif.js
@@ -31,14 +31,15 @@ router.patch("/Notif/:id",async(req,res)=>{
 
 router.patch("/msgNotif/:id", async (req, res) => {
     try {
-      const { message } = req.body;
-      const updateNotif = await Notif.findByIdAndUpdate(req.params.id, { message });
+      const { message } = req.body; // Extract the message from the request body
+      const updateNotif = await Notif.findByIdAndUpdate(req.params.id, { message }, { new: true });
       res.status(200).json(updateNotif);
     } catch (error) {
       console.log(error);
-      res.status(500).json({ error: "Internal Server Error" });
+      res.status(500).send("Error updating notification message");
     }
   });
+  
   
 
 router.patch("/Notif", async (req, res) => {
